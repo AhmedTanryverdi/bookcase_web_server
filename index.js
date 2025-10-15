7887cfd274924626b90138568a5547daba7f4fe1
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-10-15 23:02:15 +0300

    добавил использование шаблонизатора ejs

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Hello Express!");
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  const data = { title: 'Привет, мир!' };
+  res.render('index', data);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
